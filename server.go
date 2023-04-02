9e3c66a9902df0ba4942db7eaa5629593d054cb2
mmurzin <goodlost@yandex.ru>
2023-04-03 00:10:33 +0300

    rerun config

--- server.go
+++ server.go
@@ -7,12 +7,12 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-const PORT = 80
+const PORT = 8080
 
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "{\"gretting\": \"Hello, World!\"}")
+		return c.String(http.StatusOK, "{\"gretting\": \"Hello, World!!\"}")
 	})
 	e.Logger.Fatal(e.Start(":" + strconv.FormatInt(PORT, 10)))
 }
