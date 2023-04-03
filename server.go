cd5a944a1d6e531af9eec5000ac1be83da47bd79
mmurzin <goodlost@yandex.ru>
2023-04-03 10:00:06 +0300

    imports

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"golang.org/x/crypto/acme/autocert"
 )
 
@@ -11,11 +12,11 @@ const PORT = 8080
 
 func main() {
 	e := echo.New()
-	// e.AutoTLSManager.HostPolicy = autocert.HostWhitelist("<DOMAIN>")
+	e.AutoTLSManager.HostPolicy = autocert.HostWhitelist("murzinma.ru")
 	// Cache certificates to avoid issues with rate limits (https://letsencrypt.org/docs/rate-limits)
 	e.AutoTLSManager.Cache = autocert.DirCache("/var/www/.cache")
-	//e.Use(middleware.Recover())
-	//e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
 	e.GET("/", func(c echo.Context) error {
 		return c.HTML(http.StatusOK, `
 			<h1>Welcome to Echo!</h1>
