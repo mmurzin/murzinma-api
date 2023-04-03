331014bade8e8c334b173df905c35aa179608bca
mmurzin <goodlost@yandex.ru>
2023-04-03 09:29:34 +0300

    try https

--- server.go
+++ server.go
@@ -2,17 +2,26 @@ package main
 
 import (
 	"net/http"
-	"strconv"
 
 	"github.com/labstack/echo/v4"
+	"golang.org/x/crypto/acme/autocert"
 )
 
 const PORT = 8080
 
 func main() {
 	e := echo.New()
+	// e.AutoTLSManager.HostPolicy = autocert.HostWhitelist("<DOMAIN>")
+	// Cache certificates to avoid issues with rate limits (https://letsencrypt.org/docs/rate-limits)
+	e.AutoTLSManager.Cache = autocert.DirCache("/var/www/.cache")
+	//e.Use(middleware.Recover())
+	//e.Use(middleware.Logger())
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "{\"gretting\": \"Hello, World!!\"}")
+		return c.HTML(http.StatusOK, `
+			<h1>Welcome to Echo!</h1>
+			<h3>TLS certificates automatically installed from Let's Encrypt :)</h3>
+		`)
 	})
-	e.Logger.Fatal(e.Start(":" + strconv.FormatInt(PORT, 10)))
+
+	e.Logger.Fatal(e.StartAutoTLS(":443"))
 }
